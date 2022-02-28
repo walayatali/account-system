59fd9339f743ab20321b5df47e758737cf7d0e17
walayatali <walayat.ali@xprolabs.com>
2022-02-28 15:37:46 +0500

    Updated Validation logic for reducer hook and added errors and messages in appopriate order

--- src/Components/Accounts/FilterReport.js
+++ src/Components/Accounts/FilterReport.js
@@ -3,45 +3,40 @@ import Card from '../UI/Card';
 import classes from './FilterReport.module.css';
 
 const filterReducer = (state, action) => {
+
 	
-	if (action.type == 'DATE_VALIDATE') {
+	
+	if (action.type == 'VALIDATE_FILTER') {
+		
 		let currThisDate = new Date();
 		let currDate =  currThisDate.toISOString().split('T')[0];
-		if (action.filterDate.trim() === "")
+
+		let actionDate = action.filterDate.trim();
+		let actionPrice = action.filterPrice.trim();
+		
+		if (actionDate !== "" && actionPrice !== "")
 		{
-			return {
-				filterDate: action.filterDate, filterPrice: state.filterPrice, filterValid: false
+			if ((actionDate !== currDate) && (actionPrice > 0)) {
+				return {
+					filterDate: action.filterDate, filterPrice: state.filterPrice, filterValid: true
+				}	
+			}
+			if (actionPrice < 0 ){
+				return {
+					error: true, errorMessage: 'Price can not be negative number'
+				}	
 			}	
-		}
-		else if (action.filterDate.trim() === currDate)
-		{
-			return {
-				filterDate: action.filterDate, filterPrice: state.filterPrice,filterValid: false
-			}	
-		}
-		else{
-			return {
-				filterDate: action.filterDate, filterPrice: state.filterPrice, filterValid: true
-			}	
-		}
-	}
-	else if (action.type == 'PRICE_VALIDATE') {
-		if (action.filterPrice.trim() === "")
-		{
-			return {
-				filterDate: state.filterDate, filterPrice: action.filterPrice,filterValid: false
-			}	
-		}
-		else if (action.filterPrice.trim() < 0)
-		{
-			return {
-				filterDate: state.filterDate, filterPrice: action.filterPrice, filterValid: false
-			}	
-		}
-		else{
-			return {
-				filterDate: state.filterDate, filterPrice: action.filterPrice, filterValid: true
-			}	
+		}else{
+			if (actionDate === '' && actionPrice === ''){
+				return {
+					error: true, errorMessage: 'Enter Date or Price to continue'
+				}	
+			}
+			if (actionPrice < 0 ){
+				return {
+					error: true, errorMessage: 'Price can not be negative number'
+				}	
+			}
 		}
 	}
 	
@@ -53,12 +48,8 @@ function FilterReport(props)	{
 	const priceRef = useRef();
 	const dateRef = useRef();
 	
-	const filterDateChangeHandler = () => {
-		dispatch({type: "DATE_VALIDATE", filterDate: dateRef.current.value})
-	}
-
-	const filterPriceChangeHandler = () => {
-		dispatch({type: "PRICE_VALIDATE", filterPrice: priceRef.current.value})
+	const filterChangeHandler = () => {
+		dispatch({type: "VALIDATE_FILTER", filterDate: dateRef.current.value, filterPrice: priceRef.current.value})
 	}
 
 	const submitHandler = (e) => {
@@ -66,23 +57,24 @@ function FilterReport(props)	{
 		props.onFilterExpenses(dateRef.current.value, priceRef.current.value);
 	}
 
-	const InitialFilter = {filterDate : Date.now(), filterPrice: "", filterValid: false};
+	const InitialFilter = {filterDate : Date.now(), filterPrice: "", filterValid: false, error: false, errorMessage:''};
 	const [filterState, dispatch] =  useReducer(filterReducer, InitialFilter);
 	
 	return (
 		<Card>
 			<div className={classes['new-filter']}>
 				<form onSubmit = {submitHandler}>
+					{filterState.error && <p className={classes.error}>{filterState.errorMessage}</p>}
 					<div className={classes['new-filter__controls']}>
 						<div className={classes['new-filter__control']}>
 							<label>Filter By Date</label>
-							<input ref={dateRef} onChange={filterDateChangeHandler} type="date" />
+							<input ref={dateRef} onChange={filterChangeHandler} type="date" />
 						</div>
 					</div>
 					<div className={classes['new-filter__controls']}>
 						<div className={classes['new-filter__control']}>
 							<label>Filter By Price</label>
-							<input ref={priceRef} onChange={filterPriceChangeHandler} type="text" />
+							<input ref={priceRef} onChange={filterChangeHandler} type="text" />
 						</div>
 					</div>
 					<div className={classes['new-filter__actions']}>
